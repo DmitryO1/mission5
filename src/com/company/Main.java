eb08d41e5b973effc899cfcaaa30237650a8c949
DmitryO <ordyntcev.dmitry@e-queo.com>
2016-10-24 14:39:24 +0300

    Mission2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,48 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
+        import java.io.*;
+    import java.util.*;
+    import java.text.*;
+    import java.math.*;
+    import java.util.regex.*;
+
+        public class Solution {
+
+            public static void main(String[] args) {
+
+                Scanner sc=new Scanner(System.in);
+                int n=sc.nextInt();
+                String ans="";
+                if(n>=1 && n<=100){
+                    ans = "Weird";
+                    if (n%2==1){
+                        ans = "Weird";
+                    }
+                    else if(n>2 && n<5){
+                        ans = "Not Weird";
+                    }
+                    else if(n>20) {
+                        ans = "Not Weird";
+                    }
+                    else if(n>6 && n<20){
+                        if (n%2==0){
+                            ans = "Weird";
+                        }
+                        else
+                            ans = "Not Weird";
+                    }
+
+                }
+
+                System.out.println(ans);
+
+
+            }
+        }
     }
 }
